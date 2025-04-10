5e4f7d52dee6beb4bca6c8a8d0e93c5d87f4bf1c
Александр Зайцев <sdjgsinhzoghaodg@yandex.ru>
2025-04-10 14:00:44 +0300

    connected scss

--- main.js
+++ main.js
@@ -13,48 +13,48 @@ let firstPlayerTurn = true;
 // Clear cells function
 
 function clearCells() {
-  let marks = document.getElementsByClassName("cell__mark");
+    let marks = document.getElementsByClassName("playground__mark");
 
-  if (marks.length > 0) {
-    for (let i = 0; marks.length != 0; i++) {
-      marks[0].remove();
+    if (marks.length > 0) {
+        for (let i = 0; marks.length != 0; i++) {
+            marks[0].remove();
+        }
     }
-  }
 }
 
 // Start button listener
 
 startButton.addEventListener("click", () => {
-  if (gameStarted) {
-    startButton.textContent = "Start game";
-    clearCells();
-    gameStarted = false;
-    modal.style.display = "flex";
-  } else {
-    startButton.textContent = "Retry";
-    clearCells();
-    gameStarted = true;
-    modal.style.display = "none";
-  }
+    if (gameStarted) {
+        startButton.textContent = "Start game";
+        clearCells();
+        gameStarted = false;
+        modal.style.display = "flex";
+    } else {
+        startButton.textContent = "Retry";
+        clearCells();
+        gameStarted = true;
+        modal.style.display = "none";
+    }
 });
 
 // Add mark function
 
 playgroundCells.forEach((element) => {
-  element.addEventListener("click", function () {
-    if (gameStarted) {
-      if (!element.firstChild) {
-        let newMark = document.createElement("div");
-        element.append(newMark);
-        newMark.classList.add("cell__mark");
-        if (firstPlayerTurn) {
-          newMark.classList.add("x-mark");
-          firstPlayerTurn = false;
-        } else {
-          newMark.classList.add("o-mark");
-          firstPlayerTurn = true;
+    element.addEventListener("click", function () {
+        if (gameStarted) {
+            if (!element.firstChild) {
+                let newMark = document.createElement("div");
+                element.append(newMark);
+                newMark.classList.add("playground__mark");
+                if (firstPlayerTurn) {
+                    newMark.classList.add("x-mark");
+                    firstPlayerTurn = false;
+                } else {
+                    newMark.classList.add("o-mark");
+                    firstPlayerTurn = true;
+                }
+            }
         }
-      }
-    }
-  });
+    });
 });
